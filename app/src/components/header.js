8563d9cd3e212924440b98929f2ea5769f93ff35
ukaushik-98 <email@example.com>
2022-01-22 20:36:33 -0800

    adding blog page

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 
-const pages = ['Home', 'Personal Projects', 'Resume'];
+const pages = ['Home', 'Projects', 'Resume', 'Blog'];
 
 const ResponsiveAppBar = () => {
   return (
