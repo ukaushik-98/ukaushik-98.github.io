75c539e7c575660adf7cf313f53ec289d60c4005
ukaushik-98 <email@example.com>
2022-01-22 18:41:17 -0800

    creating nav bar

---
+++ app/src/components/header.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+const pages = ['Home', 'Projects', 'Resume'];
+
+const ResponsiveAppBar = () => {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar >
+          <Box sx={{ flexGrow: 1, 
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                // onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
